a540b2be9b276d345811ffb05d71475e8035293a
Łukasz Kostrzewa <lukko98@gmail.com>
2020-03-17 21:33:31 +0100

    Initial Commit

---
+++ ApplicationCore/src/main/java/pl/lodz/p/it/tks/App.java
@@ -0,0 +1,13 @@
+package pl.lodz.p.it.tks;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

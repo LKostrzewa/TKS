faeadc9ba1f2cad4b9f45d0a2fe479d837279829
Łukasz Kostrzewa <lukko98@gmail.com>
2020-03-18 17:52:27 +0100

    moved repositories to RepositoryAdapters (different types errors show up)

--- RepositoryAdapters/src/main/java/pl/lodz/p/it/tks/repository/ReservationRepository.java
+++ RepositoryAdapters/src/main/java/pl/lodz/p/it/tks/repository/ReservationRepository.java
@@ -1,28 +1,28 @@
 package pl.lodz.p.it.tks.repository;
 
 import org.springframework.stereotype.Repository;
-import pl.lodz.p.it.tks.model.Reservation;
+import pl.lodz.p.it.tks.data.ReservationEnt;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Repository
-public class ReservationRepository extends RepositoryTemplate<Reservation> {
+public class ReservationRepository extends RepositoryTemplate<ReservationEnt> {
 
-    public List<Reservation> getReservationsForClient(String login){
+    public List<ReservationEnt> getReservationsForClient(String login){
         return getAll().stream().filter(
                 r -> r.getClient().getLogin().equals(login))
                 .collect(Collectors.toList());
     }
 
-    public List<Reservation> getReservationsForResource(String id){
+    public List<ReservationEnt> getReservationsForResource(String id){
         return getAll().stream().filter(
                 r -> r.getResource().getId().equals(id))
                 .collect(Collectors.toList());
     }
 
-    public Optional<Reservation> getReservedReservations(String id){
+    public Optional<ReservationEnt> getReservedReservations(String id){
         return getReservationsForResource(id).stream().filter(
                 r -> r.getEnding() == null).findAny();
     }
---
+++ RepositoryAdapters/src/main/java/pl/lodz/p/it/tks/repository/ResourceRepository.java
@@ -0,0 +1,27 @@
+package pl.lodz.p.it.tks.repository;
+
+import org.springframework.stereotype.Repository;
+import pl.lodz.p.it.tks.data.BallRoomEnt;
+import pl.lodz.p.it.tks.data.ResourceEnt;
+import pl.lodz.p.it.tks.data.TableEnt;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class ResourceRepository extends RepositoryTemplate<ResourceEnt> {
+
+    public List<TableEnt> getAllTables(){
+        List<TableEnt> list = new ArrayList<>();
+        getAll().stream().filter(t -> t instanceof TableEnt)
+                .forEach(t -> list.add((TableEnt) t));
+        return list;
+    }
+
+    public List<BallRoomEnt> getAllBallRooms(){
+        List<BallRoomEnt> list = new ArrayList<>();
+        getAll().stream().filter(b -> b instanceof BallRoomEnt)
+                .forEach(b -> list.add((BallRoomEnt) b));
+        return list;
+    }
+}

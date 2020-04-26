cc77b12fa0409e59499770a944cd92be64640eb6
pawelbialek <pawelbialek98@icloud.com>
2020-04-26 20:38:55 +0200

    Dodano post i getAll i uniwersalny get w SOAP

--- SoapModel/src/main/java/com/baeldung/springsoap/gen/PostBallRoomRequest.java
+++ SoapModel/src/main/java/com/baeldung/springsoap/gen/PostBallRoomRequest.java
@@ -11,22 +11,24 @@ package com.baeldung.springsoap.gen;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlSeeAlso;
+import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
 
 /**
- * <p>Java class for resourceSOAP complex type.
+ * <p>Java class for anonymous complex type.
  * 
  * <p>The following schema fragment specifies the expected content contained within this class.
  * 
  * <pre>
- * &lt;complexType name="resourceSOAP">
+ * &lt;complexType>
  *   &lt;complexContent>
  *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
  *       &lt;sequence>
  *         &lt;element name="id" type="{http://www.w3.org/2001/XMLSchema}string"/>
  *         &lt;element name="price" type="{http://www.w3.org/2001/XMLSchema}double"/>
+ *         &lt;element name="description" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="numOfRooms" type="{http://www.w3.org/2001/XMLSchema}int"/>
  *       &lt;/sequence>
  *     &lt;/restriction>
  *   &lt;/complexContent>
@@ -36,19 +38,21 @@ import javax.xml.bind.annotation.XmlType;
  * 
  */
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "resourceSOAP", propOrder = {
+@XmlType(name = "", propOrder = {
     "id",
-    "price"
+    "price",
+    "description",
+    "numOfRooms"
 })
-@XmlSeeAlso({
-    BallRoomSOAP.class,
-    TableSOAP.class
-})
-public class ResourceSOAP {
+@XmlRootElement(name = "postBallRoomRequest")
+public class PostBallRoomRequest {
 
     @XmlElement(required = true)
     protected String id;
     protected double price;
+    @XmlElement(required = true)
+    protected String description;
+    protected int numOfRooms;
 
     /**
      * Gets the value of the id property.
@@ -88,6 +92,46 @@ public class ResourceSOAP {
      */
     public void setPrice(double value) {
         this.price = value;
+    }
+
+    /**
+     * Gets the value of the description property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * Sets the value of the description property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setDescription(String value) {
+        this.description = value;
+    }
+
+    /**
+     * Gets the value of the numOfRooms property.
+     * 
+     */
+    public int getNumOfRooms() {
+        return numOfRooms;
+    }
+
+    /**
+     * Sets the value of the numOfRooms property.
+     * 
+     */
+    public void setNumOfRooms(int value) {
+        this.numOfRooms = value;
     }
 
 }
--- SoapModel/src/main/java/com/baeldung/springsoap/gen/PostTableResponse.java
+++ SoapModel/src/main/java/com/baeldung/springsoap/gen/PostTableResponse.java
@@ -11,22 +11,21 @@ package com.baeldung.springsoap.gen;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlSeeAlso;
+import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
 
 /**
- * <p>Java class for resourceSOAP complex type.
+ * <p>Java class for anonymous complex type.
  * 
  * <p>The following schema fragment specifies the expected content contained within this class.
  * 
  * <pre>
- * &lt;complexType name="resourceSOAP">
+ * &lt;complexType>
  *   &lt;complexContent>
  *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
  *       &lt;sequence>
  *         &lt;element name="id" type="{http://www.w3.org/2001/XMLSchema}string"/>
- *         &lt;element name="price" type="{http://www.w3.org/2001/XMLSchema}double"/>
  *       &lt;/sequence>
  *     &lt;/restriction>
  *   &lt;/complexContent>
@@ -36,19 +35,14 @@ import javax.xml.bind.annotation.XmlType;
  * 
  */
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "resourceSOAP", propOrder = {
-    "id",
-    "price"
+@XmlType(name = "", propOrder = {
+    "id"
 })
-@XmlSeeAlso({
-    BallRoomSOAP.class,
-    TableSOAP.class
-})
-public class ResourceSOAP {
+@XmlRootElement(name = "postTableResponse")
+public class PostTableResponse {
 
     @XmlElement(required = true)
     protected String id;
-    protected double price;
 
     /**
      * Gets the value of the id property.
@@ -72,22 +66,6 @@ public class ResourceSOAP {
      */
     public void setId(String value) {
         this.id = value;
-    }
-
-    /**
-     * Gets the value of the price property.
-     * 
-     */
-    public double getPrice() {
-        return price;
-    }
-
-    /**
-     * Sets the value of the price property.
-     * 
-     */
-    public void setPrice(double value) {
-        this.price = value;
     }
 
 }

7a8465d21aa51682ed1e2b467eb4cb0942e52957
216804 <216804@edu.p.lodz.pl>
2020-06-12 18:27:57 +0200

    added publishing users to rabbitMQ

---
+++ Publisher/src/main/java/pl/lodz/p/it/tks/publisherrabbitmq/PublisherMQ.java
@@ -0,0 +1,24 @@
+package pl.lodz.p.it.tks.publisherrabbitmq;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PublisherMQ {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    @Autowired
+    public PublisherMQ(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @PostMapping
+    public void publishUser(@RequestBody UserPayload userPayload) {
+        rabbitTemplate.convertAndSend("add-user", userPayload);
+    }
+}
